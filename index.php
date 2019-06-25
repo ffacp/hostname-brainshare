18f3ad501a31b0b6b108b4652ac84055613f62ca
Florian Fröhlich <44257732+ffsws@users.noreply.github.com>
2019-06-25 22:55:51 +0200

    init

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+	<body>
+                Diese Seite läuft auf POD: <b>
+<?php
+$hostname = gethostname();
+print $hostname;
+		$secondsWait = 0.1;
+header("Refresh:$secondsWait");
+
+		
+
+if (strpos($hostname, 'blue') !== false) {
+echo "<body style='background-color:blue'>";
+};
+		
+if (strpos($hostname, 'green') !== false) {
+echo "<body style='background-color:green'>";
+};
+		
+?>
+
+
+Brainshare 2019
+</b>
+	</body>
+</html>
+
